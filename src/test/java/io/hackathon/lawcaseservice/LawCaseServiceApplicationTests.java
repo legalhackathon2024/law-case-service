fde68c9f25eadc98cfd877fedc74a95df070d4ee
Grzegorz Oladele <grzegorzoladele@MacBook-Pro-Grzegorz.local>
2024-03-08 21:27:52 +0100

    Init project

---
+++ src/test/java/io/hackathon/lawcaseservice/LawCaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hackathon.lawcaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawCaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
